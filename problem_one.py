322e26f3bf13164a744dea7747661acfa9092234
Darren Cobian <darrencobian98@gmail.com>
2021-12-25 18:50:13 -0800

    working code

--- problem_one.py
+++ problem_one.py
@@ -1,13 +1,14 @@
 def main():
     # arguments
-    numList = [10, 15, 3, 7]
+    numList = [0]
     sum_k = 17
 
     placekeeper = 0
     for i in range(len(numList)):
         if numList[placekeeper] + numList[i] == sum_k and placekeeper != i:
             return True
-
+        elif i == 3:
+            placekeeper = placekeeper + 1
     return False
 
 if __name__ == '__main__':
