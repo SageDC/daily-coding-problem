06da59a62915de51e6fde9b3a95ca4df8dc7bce3
Darren Cobian <darrencobian98@gmail.com>
2021-12-27 12:31:06 -0800

    Uber (Hard) problem solved

--- problem_two.py
+++ problem_two.py
@@ -1,22 +1,24 @@
 # Question asked by Uber (Hard)
 
 def main():
-    # arguments
-    numList1 = [1, 2, 3, 4, 5]
-    numList2 = [3, 2, 1]
+    # argument
+    numList = [1, 2, 3, 4, 5]
+    resList = [None]*len(numList)
 
-    position = 0
+    # base case
+    if len(numList) == 0 or len(numList) == 1:
+        return numList
+
     product = 1
-    for i in range(len(numList1)):
-        # base cases
-        if len(numList1) == 0 or len(numList1) == 1:
-            return numList1
-        elif position != i and i != len(numList1):
-            product = product * numList1[i]
-        elif i == len(numList1) - 1:
-            position = position + 1
-        numList1[position] = product
-    return numList1
+    for i in range(len(numList)):
+        for j in range(len(numList)):
+            if(i != j):
+                product = product * numList[j]
+        resList[i] = product
+        product = 1
+
+    return resList
+
 
 if __name__ == '__main__':
     print(main())
